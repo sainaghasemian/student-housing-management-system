12ac6c1e4fb7d90d48494abefe5be3a72e1315e3
Saina Ghasemian <sainaghasemian@gmail.com>
2022-11-28 21:50:59 -0700

    add files

---
+++ PHP_MySQL_Simple_Project/Users Database.sql
@@ -0,0 +1,45 @@
+﻿# SQL-Front 5.1  (Build 4.16)
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+
+# Host: localhost    Database: 471
+# ------------------------------------------------------
+# Server version 5.0.51b-community-nt-log
+
+DROP DATABASE IF EXISTS `471`;
+CREATE DATABASE `471` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `471`;
+
+#
+# Source for table users
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL auto_increment,
+  `Name` varchar(255) default NULL,
+  `Email` varchar(255) default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table users
+#
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
---
+++ PHP_MySQL_Simple_Project/Website/index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="add.php" method="post">
+   Name: <input type="text" name="name"><br>
+   E-mail: <input type="text" name="email"><br>
+   <input type="submit" value="add">
+</form>
+
+</body>
+</html>
