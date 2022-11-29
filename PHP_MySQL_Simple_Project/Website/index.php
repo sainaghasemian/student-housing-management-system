f11ec12b8b4d53b1dab34df15f977e06fd443d60
Saina Ghasemian <sainaghasemian@gmail.com>
2022-11-28 23:15:51 -0700

    style

--- PHP_MySQL_Simple_Project/Website/index.php
+++ PHP_MySQL_Simple_Project/Website/index.php
@@ -1,6 +1,33 @@
 <html>
-<body>
 
+<style type="style/css">
+	body{
+		text-align: center;
+		padding-bottom: 100px;
+		padding-top: 50px;
+	}
+	#footer {
+	   position:fixed;
+	   bottom:0;
+	   width:100%;
+	   height:60px;
+	   background:#6cf;
+	}
+	header{
+		position: absolute;
+		top:0;
+		width: 100%;
+		height: 60px;
+		background: #6af;
+		padding-bottom: 20px;
+	}
+</style>
+
+<header>
+	<h1> University Housing Database System </h1>
+</header>
+
+<body>
 <form action="add.php" method="post">
    Name: <input type="text" name="name"><br>
    E-mail: <input type="text" name="email"><br>
@@ -9,3 +36,5 @@
 
 </body>
 </html>
+
+
