fd7c0b17dd9b528250d73a5c3aee9ca6efeb3fe3
Saina Ghasemian <sainaghasemian@gmail.com>
2022-12-01 11:39:09 -0700

    change to staff login

--- stafflogin.php
+++ stafflogin.php
@@ -28,7 +28,6 @@
 </header>
 <body>
 	<form action="stafflogin.php" method="post">
-	Type "A for Admin login, or type "M" for Mental Health Advisor: <input type="text" name="identifyerID" required><br><br>
 	Employee ID: <input type="text" name="employeeID" required><br><br>
 	Password: <input type="password" name="Password" required><br><br>
 	<input type="button" name = "Login As Mental Health Advisor" value="Login As Mental Health Advisor" /> 
