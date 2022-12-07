2de300e1fb965c8f8989eb22eb6c74c2ab426786
Isaiah Lemieux <isaiah.lemieux@ucalgary.ca>
2022-12-07 01:01:08 -0700

    Update bookMHappSQL.php

--- bookMHappSQL.php
+++ bookMHappSQL.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'databaseConnect.php';
 
 session_start();
@@ -7,17 +8,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$advisorID = $_POST["Advisor_ID"];
-$appointmentID = $_POST["Appointment_ID"];
-$date = $_POST["DateN"];
-$time = $_POST["TimeN"];
-$user_ID = $_SESSION['studentID']; 
+
+$appointmentID = $_POST["appointmentID"];
+
+$result = mysqli_query($conn, "SELECT * FROM APPOINTMENTS WHERE Appointment_ID = '$appointmentID'");
+
+$row = mysqli_fetch_array($result);
+
+$advisorID = $row[0];
+$date = $row[2];
+$time = $row[3];
+$user_ID = $_SESSION['studentID'];
+
 
 mysqli_query($conn, "DELETE FROM APPOINTMENTS WHERE Appointment_ID = '$appointmentID'");
 
-mysqli_query($conn, "INSERT INTO `Appointments` ('Advisor_ID','Appointment_ID`, 'DateN', 'TimeN', 'studentID') 
-VALUES ('$advisorID', '$appointmentID', '$date', '$time', '$user_ID', 'Mental Health Appointment has been booked successfully')");
+mysqli_query($conn, "INSERT INTO `Appointments` (`Advisor_ID`,`Appointment_ID`, `Date`, `Time`, `Student_ID`) 
+VALUES ('$advisorID', '$appointmentID', '$date', '$time', '$user_ID')");
 
 mysqli_close($conn);
-echo '<br> <a href="bookMHapp.php">Back</a> <br>';
+
+header("Location: appointmentsMainPg.php");
 ?>
